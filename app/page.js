8b146a1bab890e42aef7d1dfc5318b6547042be7
Sahil <sk2061899@gmail.com>
2024-11-01 21:36:23 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,141 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+import styles from './page.module.css'
+
+const page = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    number: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("Submitting form...");
+
+    const formDataToSend = new FormData();
+    for (const key in formData) {
+      formDataToSend.append(key, formData[key]);
+    }
+
+    try {
+      console.log(
+        "Form Data to Send:",
+        Object.fromEntries(formDataToSend.entries())
+      );
+      console.log("Uploading data...");
+      const response = await fetch(
+        // "https://m.designindianhomes.com/submitForm",
+        "/api/submit-form",
+        {
+          method: "POST",
+          // mode: "no-cors",
+          body: formDataToSend,
+        }
+      );
+      console.log("response: ", response);
+      console.log("Response status:", response.status);
+      console.log("Response headers:", response.headers);
+      console.log("Response body:", await response.text());
+
+      if (response.ok) {
+        console.log("Form data submitted successfully!");
+        console.log(
+          "Form Data to Send:",
+          Object.fromEntries(formDataToSend.entries())
+        );
+
+      } else {
+        console.error("Form data submission failed. Response:", response);
+      }
+    } catch (error) {
+      console.error("Error during form data submission:", error);
+    }
+  };
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <form className={styles.main} method="post" onSubmit={handleSubmit}>
+          {/* Your form elements go here */}
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700 ml-2 my-2"
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={handleChange}
+            className="mt-1 p-2 border-2 rounded-full w-full"
+            required
+          />
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700 ml-2 mt-4 mb-2"
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={handleChange}
+            className="mt-1 p-2 border-2 rounded-full w-full"
+            required
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <label
+            htmlFor="number"
+            className="block text-sm font-medium text-gray-700 ml-2 mt-4 mb-2"
+          >
+            Contact Number
+          </label>
+          <input
+            type="tel"
+            id="number"
+            name="number"
+            onChange={handleChange}
+            className="mt-1 p-2 border-2 rounded-full w-full"
+            required
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-gray-700 ml-2 mt-4 mb-2"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            onChange={handleChange}
+            className="mt-1 p-2 border-2 rounded-full w-full text-sm"
+            required
+          ></textarea>
+
+          {/* Add other form fields similarly */}
+
+          <button
+            type="submit"
+            className="bg-gray-900 text-white py-2 px-4 mt-4 rounded-full w-full hover:bg-gray-700 hover:shadow"
+          >submit
+          </button>
+        </form>
     </div>
   );
-}
+};
+
+
+export default page;
